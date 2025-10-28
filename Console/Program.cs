ebdaf5cb255d000922f09cc2ee8f51765f746839
Egen Stephens <estephens6@huskers.unl.edu>
2025-10-27 22:25:23 -0500

    Fix Power test and polish workflow

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,91 +1,141 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace GithubActionsLab;
 
-namespace GithubActionsLab;
-
-public class Program
+[TestClass]
+public class Addition
 {
-	static void Main(string[] args)
-	{
-		Console.WriteLine("The Quick Calculator");
-		var loop = true;
-		while (loop)
-		{
-			try
-			{
-				Func<string, string, double> operation;
-				Console.WriteLine("1) Add (x+y)");
-				Console.WriteLine("2) Subtract (x-y)");
-				Console.WriteLine("3) Multiply (x*y)");
-				Console.WriteLine("4) Divide (x/y)");
-				Console.WriteLine("5) Power (x^y)");
-				Console.WriteLine("6) Quit");
-				var operationSelection = GetInput("Select your operation: ");
-				switch (operationSelection)
-				{
-					case "1":
-						operation = Add;
-						break;
-					case "2":
-						operation = Subtract;
-						break;
-					case "3":
-						operation = Multiply;
-						break;
-					case "4":
-						operation = Divide;
-						break;
-					case "5":
-						operation = Power;
-						break;
-					case "6":
-						loop = false;
-						continue;
-					default:
-						throw new ArgumentException("You did not select a valid option!");
-				}
+    [TestMethod]
+    public void Add_Valid_Patino()
+    {
+        Assert.AreEqual(3, Program.Add("1", "2"));
+        Assert.AreEqual(5, Program.Add("3", "2"));
+        Assert.AreEqual(12, Program.Add("5", "7"));
+    }
 
-				var x = GetInput("Enter x: ");
-				var y = GetInput("Enter y: ");
-				var result = operation(x, y);
-				Console.WriteLine($"Result: {result}");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-			}
-		}
-	}
+    [TestMethod]
+    public void Add_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
+    }
 
-	private static string GetInput(string prompt)
-	{
-		Console.Write(prompt);
+    [TestMethod]
+    public void Add_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+    }
+}
 
-		return Console.ReadLine()?.Trim() ?? throw new InvalidOperationException();
-	}
+[TestClass]
+public class Subtraction
+{
+    [TestMethod]
+    public void Subtract_Valid_Patino()
+    {
+        Assert.AreEqual(-1, Program.Subtract("1", "2"));
+        Assert.AreEqual(1, Program.Subtract("3", "2"));
+        Assert.AreEqual(-2, Program.Subtract("5", "7"));
+    }
 
-	public static double Add(string x, string y)
-	{
-		return double.Parse(x) + double.Parse(y);
-	}
+    [TestMethod]
+    public void Subtract_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+    }
 
-	public static double Subtract(string x, string y)
-	{
-		return double.Parse(x) - double.Parse(y);
-	}
+    [TestMethod]
+    public void Subtract_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+    }
+}
 
-	public static double Multiply(string x, string y)
-	{
-		return double.Parse(x) * double.Parse(y);
-	}
+[TestClass]
+public class Multiplication
+{
+    [TestMethod]
+    public void Multiply_Valid_Patino()
+    {
+        Assert.AreEqual(2, Program.Multiply("1", "2"));
+        Assert.AreEqual(6, Program.Multiply("3", "2"));
+        Assert.AreEqual(35, Program.Multiply("5", "7"));
+    }
 
-	public static double Divide(string x, string y)
-	{
-		return double.Parse(x) / double.Parse(y);
-	}
+    [TestMethod]
+    public void Multiply_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+    }
 
-	// Implement this method following a similar pattern as above
-	public static double Power(string x, string y)
-	{
-		return Math.Pow(double.Parse(x), double.Parse(y));
-	}
+    [TestMethod]
+    public void Multiply_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+    }
+}
+
+[TestClass]
+public class Division
+{
+    [TestMethod]
+    public void Divide_Valid_Patino()
+    {
+        Assert.AreEqual(0.5, Program.Divide("1", "2"), 0.0001);
+        Assert.AreEqual(5, Program.Divide("10", "2"), 0.0001);
+        Assert.AreEqual(1, Program.Divide("5", "5"), 0.0001);
+    }
+
+    [TestMethod]
+    public void Divide_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+    }
+
+    [TestMethod]
+    public void Divide_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+    }
+}
+
+[TestClass]
+public class Power
+{
+    [TestMethod]
+    public void Power_Valid_Patino()
+    {
+        Assert.AreEqual(1, Program.Power("1", "0"));
+        Assert.AreEqual(100, Program.Power("10", "2"));
+        Assert.AreEqual(0, Program.Power("0", "5"));
+    }
+
+    [TestMethod]
+    public void Power_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+    }
+
+    [TestMethod]
+    public void Power_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+    }
 }
